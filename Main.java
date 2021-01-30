848ddd71dbcd73d254fa67e59dd5772aad5d2b7c
Kaewphet Nuruang <66639008+nobiofla@users.noreply.github.com>
2021-01-30 21:20:39 +0700

    Update Input Variable
    int a, x, y, z
    int[] c

--- Main.java
+++ Main.java
@@ -3,8 +3,19 @@ import java.util.Scanner;
 public class Main {
     static Scanner scn = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("IHATELOVEFISHSTOP");
-        System.out.println("ISUSPRAหยุด");
+        int a, x , y , z;
+        int[] b, c;
+
+        a = scn.nextInt();
+        c = new int[a];
+
+        x = scn.nextInt();
+        y = scn.nextInt();
+        z = scn.nextInt();
+
+        for(int i = 0; i < a; i++) {
+            c[i] = scn.nextInt();
+        }
     }
     
 }
