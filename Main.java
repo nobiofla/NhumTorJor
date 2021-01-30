5053cd0ece81d05ce83b730b33af44e41ec9cd3a
ItsMnZz <76113873+ItsMnZz@users.noreply.github.com>
2021-01-30 20:45:44 +0700

    UPDATEHUMANITY
    SEXY

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class Main {
     static Scanner scn = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        System.out.println("IHATELOVEFISHSTOP");
     }
 }
