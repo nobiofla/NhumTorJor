9a379c482e9a470e64108467fb98aff7cf1800cf
Kaewphet Nuruang <66639008+nobiofla@users.noreply.github.com>
2021-01-31 20:56:26 +0700

    Update Monster_Hunter
    At least one

--- Main2.java
+++ Main2.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class Main2 {
     static Scanner scn = new Scanner(System.in);
     public static void main(String[] args) {
-        int n, r,testCase, xPosition = 0, rock = 0, round = 0;
+        int n, r,testCase, checkNum, xPosition = 0, rock = 0, round = 0;
         int[] x;
         
         testCase = scn.nextInt();
@@ -16,19 +15,27 @@ public class Main2 {
             x = new int[n];    //All monster position
 
             for(int i = 0; i < n; i++) {
-                //Continue to next input if there're the same
-                /* int in = scn.nextInt();
-                if(in) */
-                //Comment code
                 x[i] = scn.nextInt(); //monster 
                 xPosition += x[i];
             }
 
-            //Arrays.sort(x);
+            Arrays.sort(x);
 
-            for(int i = x.length - 1; xPosition > 0; ) {
-                Collections.max(x);
-                xPosition -= i; //when catapult shoot
+            for(int i = 0; i < x.length; i++) { //find the same element(number/monster position)
+                for (int j = i + 1; j < x.length; j++) {
+                   if(x[i] == x[j]) {
+                        xPosition -= x[i];
+                        x[i] = 0;
+                   }
+                }
+             }
+
+            for(int i = x.length - 1; xPosition > 0; i--) {
+                xPosition -= (i); //when catapult shoot
+                x[i] = 0;
+
+                //xPosition -= r;
+                
                 rock++;
             }
             System.out.println(rock);
